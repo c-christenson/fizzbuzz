0a7f730f39f79528705624caa0eecbcb3c5df50f
Carrie Christenson <christenson.carrie@gmail.com>
2015-02-05 21:31:52 +0000

    First Fizzbuzz

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,24 @@
+class Fizzbuzz
+
+
+	def is_divisible_by_three?(number)
+		number % 3 == 0
+	end
+
+	def is_divisible_by_five?(number)
+		number % 5 == 0
+	end
+
+	def is_divisible_by_fifteen?(number)
+		number % 15 == 0
+	end
+
+	def play_number(number)
+		return "fizzbuzz" if is_divisible_by_fifteen?(number)
+		return "fizz" if is_divisible_by_three?(number)
+		return "buzz" if is_divisible_by_five?(number)
+		number
+	end
+
+
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,47 @@
+require 'fizzbuzz'
+
+describe Fizzbuzz do 
+
+let(:fizzbuzz){Fizzbuzz.new}
+
+it 'knows that 1 is not divisible by 3' do 
+	expect(fizzbuzz.is_divisible_by_three?(1)).to eq false
+end
+
+it 'knows that 3 is divisible by 3' do
+	expect(fizzbuzz.is_divisible_by_three?(3)).to eq true
+end
+
+it 'knows that 1 is not divisible by 5' do 
+	expect(fizzbuzz.is_divisible_by_five?(1)).to eq false
+end
+
+it 'knows that 5 is divisible by 5' do 
+	expect(fizzbuzz.is_divisible_by_five?(5)).to eq true
+end
+
+it 'knows that 1 is not divisible by 15' do
+	expect(fizzbuzz.is_divisible_by_fifteen?(1)).to eq false
+end
+
+it 'knows that 15 is divisible by 15' do 
+	expect(fizzbuzz.is_divisible_by_fifteen?(15)).to eq true
+end
+
+it 'returns 1 for 1 when playing' do
+	expect(fizzbuzz.play_number(1)).to eq 1
+end
+
+it 'should return fizz if the number played is 3' do
+	expect(fizzbuzz.play_number(3)).to eq "fizz"
+end
+
+it 'should return buzz if the number played is 5' do
+	expect(fizzbuzz.play_number(5)).to eq "buzz"
+end
+
+it 'should return fizzbuzz if the number played is 15' do
+	expect(fizzbuzz.play_number(15)).to eq "fizzbuzz"
+end
+	
+end
